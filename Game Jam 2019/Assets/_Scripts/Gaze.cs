04f80da199cd64b38c9916448e5f7e7a7a930a84
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-02-11 17:09:39 +0800

    + Added Git LFS for large files + Added Room + Added Gaze Mechanic + Fixed Lighting

---
+++ Game Jam 2019/Assets/_Scripts/Gaze.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    public LayerMask GazeLayer;
+    public Transform Eye;
+
+    private GazeObject gazedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (Eye)
+        {
+            Gizmos.color = Color.green;
+
+            Gizmos.DrawRay(Eye.position, Eye.forward * 20);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Ray ray = new Ray(Eye.position, Eye.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, GazeLayer))
+        {
+            gazedObject = hit.collider.GetComponent<GazeObject>();
+
+            if (!gazedObject)
+            {
+                return;
+            }
+
+            gazedObject.OnGazeStart();
+        }
+        else
+        {
+            if (gazedObject)
+            {
+                gazedObject.OnGazeExit();
+                gazedObject = null;
+            }
+        }
+
+    }
+}
---
+++ Game Jam 2019/Assets/_Scripts/GazeObject.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GazeObject : MonoBehaviour
+{
+    [Header("Events")]
+    public UnityEvent UE_OnGazeStart;
+    public UnityEvent UE_OnGazeUpdate;
+    public UnityEvent UE_OnGazeEnd;
+    public UnityEvent UE_OnGazeExit;
+
+    [Header("Gaze Properties")]
+    public float GazeTime;
+    public Material OutlineMaterial;
+
+    public bool IsGazingFinished => isGazing && currentGazeTime <= 0;
+
+    private float currentGazeTime;
+    private bool isGazing;
+
+    private void Start()
+    {
+        currentGazeTime = GazeTime;
+    }
+
+    private void Update()
+    {
+        if (isGazing)
+        {
+            OnGazeUpdate();
+        }
+        else
+        {
+            if(currentGazeTime < GazeTime)
+            {
+                UpdateOutlineAlpha();
+                currentGazeTime += Time.deltaTime;
+
+                if(currentGazeTime > GazeTime)
+                {
+                    currentGazeTime = GazeTime;
+                }
+            }
+        }
+    }
+
+    private void UpdateOutlineAlpha()
+    {
+        Color currColor = GetComponent<Renderer>().material.GetColor("_OutlineColor");
+        currColor.a = 1 - (currentGazeTime / GazeTime);
+        GetComponent<Renderer>().material.SetColor("_OutlineColor", currColor);
+    }
+
+    public void OnGazeStart()
+    {
+        if (!isGazing)
+        {
+            isGazing = true;
+            UE_OnGazeStart.Invoke();
+            Debug.Log("Start");
+        }
+    }
+
+    public void OnGazeUpdate()
+    {
+        currentGazeTime -= Time.deltaTime;
+
+        UpdateOutlineAlpha();
+
+        UE_OnGazeUpdate.Invoke();
+        if (currentGazeTime <= 0)
+        {
+            currentGazeTime = 0;
+            OnGazeEnd();
+        }
+    }
+
+    public void OnGazeEnd()
+    {
+        if (isGazing)
+        {
+            isGazing = false;
+            UE_OnGazeEnd.Invoke();
+            Debug.Log("End");
+        }
+    }
+
+    public void OnGazeExit()
+    {
+        isGazing = false;
+        UE_OnGazeExit.Invoke();
+        Debug.Log("Exit");
+    }
+}
